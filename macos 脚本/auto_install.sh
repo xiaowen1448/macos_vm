cc3b12e472265cc90d3d7d8944a48e090784a537
xiaowen <wenxiaomst@outlook.com>
2025-01-14 17:41:39 +0800

    macos 新增虚拟机主机名自定义uuidgen

--- macos 脚本/auto_install.sh
+++ macos 脚本/auto_install.sh
@@ -1,1 +1,2 @@
+./random_hostname.sh
 echo "123456" | sudo -S /Volumes/Install\ macOS\ Catalina/Install\ macOS\ Catalina.app/Contents/Resources/startosinstall  --agreetolicense --forcequitapps
--- macos 脚本/random_hostname.sh
+++ macos 脚本/random_hostname.sh
@@ -1,2 +1,3 @@
 export uu_name=`uuidgen`
 echo "123456" | sudo -S scutil --set ComputerName $uu_name
+echo "123456" | sudo -S scutil --set LocalHostName $uu_name
