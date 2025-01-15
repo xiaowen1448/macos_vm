503420d22dff44f41b8529807007b082bd9c789b
xiaowen <wenxiaomst@outlook.com>
2025-01-15 12:23:09 +0800

    1.新增开机自启动随机主机名，以免克隆开机后提示主机名冲突 2.更改awk批量处理ip地址的脚本

--- bat/1批量克隆虚拟机脚本.bat
+++ bat/1批量克隆虚拟机脚本.bat
@@ -5,11 +5,15 @@ set TEMPLATE_PATH=D:\macos_vm\TemplateVM\macos10.15
 REM Set the storage path of the target VM
 set VM_BASE_PATH=D:\macos_vm\NewVM
 REM Set the number of virtual machines that need to be created
-set VM_COUNT=2
+set VM_COUNT=4
 rem  Random numbers start
 set sum=15
 set num=1
 set vmnet_ip=192.168.119.1
+set nmap_count=253
+set memsize="2048"
+set plist_path=D:\macos_vm\plist\chengpin
+set plist_num=0
 REM Perform batch creation
 for /L %%i in (1,1,%VM_COUNT%) do (
 
@@ -23,6 +27,7 @@ for /L %%i in (1,1,%VM_COUNT%) do (
 	echo  sata0:0.fileName = "macos10.15_%%i.vmdk"  >>  "!VM_DIR!\macos10.15.vmx"
 	echo  nvram = "macos10.15_%%i.nvram"  >>  "!VM_DIR!\macos10.15.vmx" 
 	echo  extendedConfigFile = "macos10.15_%%i.vmxf"   >>  "!VM_DIR!\macos10.15.vmx" 
+	echo  memsize="2048"  >>   "!VM_DIR!\macos10.15.vmx"
 	REM nvram = "macOS 10.15.nvram"  bios  
 	REM displayName = "macOS 10.15"
 	REM sata0:0.fileName = "macOS 10.15.vmdk"
@@ -37,6 +42,7 @@ for /L %%i in (1,1,%VM_COUNT%) do (
 )
 echo "All virtual machines have been created and are waiting to be started ..................."
 rem   the virtual machine to starting   please wait    .................... 
+rem   Wait for the VM to start up and obtain the IP address of the VM
 timeout /t 240 
 echo  "The IP address is being scanned, please wait    ...................."
 REM  Define the range of nmap scan addresses  for Example   192.168.119.2-10
@@ -44,14 +50,20 @@ for /f "tokens=1,2,3,4 delims=." %%a in ("%vmnet_ip%") do (
 	set /a result_d=%%d + %num%
 	set new_vmnet_ip=%%a.%%b.%%c.!result_d!
 )
-nmap !new_vmnet_ip!-%VM_COUNT%  > example.txt 
-awk -F: "/report/{ print $0 }" example.txt  | awk -F" " "{ print $NF }"  | awk -F"(" "{ print $2 }"  | awk -F")" "{ print $1 }"  > ip_list.txt
+echo !new_vmnet_ip!-%nmap_count%
+nmap !new_vmnet_ip!-%nmap_count%  > example.txt 
+awk -F: "/report/{ print $0 }" example.txt | awk -F" " "{ print $NF }"  > ip_list.txt
 echo  "IP address information is saved in this text  ip_list.txt"
 echo  "ip address  is done   ...................."
 echo  " run  ssh  going  ............................"
+rem  VM_COUNT  Number of virtual machines
+rem  Set the number of config.plist to be used
 for /f "tokens=* delims=" %%a in (ip_list.txt) do (
+	set /a plist_num+=1
 	REM   ssh  -o StrictHostKeyChecking=no  cc@192.168.122.190  '/Users/wx/auto_install.sh'
-	  start cmd /k ssh  -o StrictHostKeyChecking=no  wx@%%a  '/Users/wx/auto_install.sh'
+	ssh  -o StrictHostKeyChecking=no  wx@%%a   '/Users/wx/mount_efi.sh'
+	scp %plist_path%\config_!plist_num!.plist  wx@%%a :/Volumes/EFI/CLOVER/config.plist
+	start cmd /k ssh  -o StrictHostKeyChecking=no  wx@%%a  '/Users/wx/auto_install.sh'
 )
 echo   "start revise   JU ,Please wait  ................."
 
