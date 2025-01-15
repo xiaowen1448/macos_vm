bd1d4d722df211ca789ccc5264922cb194a978de
xiaowen <wenxiaomst@outlook.com>
2025-01-15 12:23:09 +0800

    1.新增开机自启动随机主机名，以免克隆开机后提示主机名冲突 2.更改awk批量处理ip地址的脚本

--- plist/run_plist.bat
+++ plist/run_plist.bat
@@ -1,14 +1,14 @@
 @setlocal enabledelayedexpansion
 @echo off
-set Plist_path=D:\macos_vm\plist\chengpin
+set plist_path=D:\macos_vm\plist\chengpin
 
  ssh  -o StrictHostKeyChecking=no  wx@192.168.119.156  '/Users/wx/mount_efi.sh'
  
  ssh  -o StrictHostKeyChecking=no  wx@192.168.119.157 '/Users/wx/mount_efi.sh'
  
- scp %Plist_path%\config_1.plist  wx@192.168.119.156:/Volumes/EFI/CLOVER/config.plist
+ scp %plist_path%\config_1.plist  wx@192.168.119.156:/Volumes/EFI/CLOVER/config.plist
  
- scp %Plist_path%\config_2.plist  wx@192.168.119.157:/Volumes/EFI/CLOVER/config.plist
+ scp %plist_path%\config_2.plist  wx@192.168.119.157:/Volumes/EFI/CLOVER/config.plist
  
  ssh  -o StrictHostKeyChecking=no  wx@192.168.119.156  '/Users/wx/reboot.sh' 
  
