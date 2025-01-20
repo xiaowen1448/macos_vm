b49bbda34af472b782ca3f7984a89e0bd411742c
xiaowen <wenxiaomst@outlook.com>
2025-01-20 14:47:33 +0800

    新增获取虚拟机ip方式， 持续监测ip是否获取成功 去掉nmap获取方式

---
+++ bat/rebuild_nvram.bat
@@ -0,0 +1,28 @@
+@setlocal enabledelayedexpansion
+@echo off
+set BASE_DIR=%cd%
+set TEMPLATE_PATH=%BASE_DIR%\..\TemplateVM\macos10.15
+REM Set the storage path of the target VM
+set VM_BASE_PATH=%BASE_DIR%\..\NewVM
+REM Set the number of virtual machines that need to be created
+set VM_COUNT=4
+set VMRUN_PATH="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
+rem  stop  vms 
+call stop_all_vms.bat
+rem  copy  nvram  
+for /r "%VM_BASE_PATH%" %%f in (*.*) do (
+   rem    "%VMRUN_PATH%" start "%%f" nogui
+   if /i "%%~xf"==".nvram"  (
+    rem  echo %%f
+	rem   %VMRUN_PATH%  stop  %%f nogui
+	echo "%%f starts rebuilding NVRM  ..............  "
+	copy /y  "%TEMPLATE_PATH%\macos10.15.nvram"    %%f  >> log\run.log  2>&1
+    echo "%%f  rebuilds NVRM Complete  ..............  "
+    )else (
+   rem   echo Value is unknown
+)
+)
+call start_all_vms.bat
+echo  "All virtual machines NVRM have been rebuilt ..............""
+echo "All virtual machines have been started .............."
+pause
--- bat/run_vm_clone.bat
+++ bat/run_vm_clone.bat
@@ -1,20 +1,23 @@
 @setlocal enabledelayedexpansion
 @echo off
 REM Set the template VM path
-set TEMPLATE_PATH=D:\macos_vm\TemplateVM\macos10.15
+set BASE_DIR=%cd%
+set TEMPLATE_PATH=%BASE_DIR%\..\TemplateVM\macos10.15
 REM Set the storage path of the target VM
-set VM_BASE_PATH=D:\macos_vm\NewVM
-set ISO_BASE_HOME=D:\macos_vm\iso
+set VM_BASE_PATH=%BASE_DIR%\..\NewVM
+set ISO_BASE_HOME=%BASE_DIR%\..\iso
+set IP_PATH=%BASE_DIR%\ip
 REM Set the number of virtual machines that need to be created
 set VM_COUNT=4
+set VMRUN_PATH="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
 rem  Random numbers start
 set sum=15
 set num=1
-set vmnet_ip=192.168.119.1
+rem set vmnet_ip=192.168.119.1
 set nmap_count=253
 rem  memory size 
 set memsize="2048"
-set plist_path=D:\macos_vm\plist\chengpin
+set plist_path=%BASE_DIR%\..\plist\chengpin
 set plist_num=0
 echo  "" > log\run.log
 REM Perform batch creation
@@ -24,49 +27,61 @@ for /L %%i in (1,1,%VM_COUNT%) do (
 	set /a sum=!sum!+%%i
     mkdir !VM_DIR!
     REM Copy the template VM file
-    xcopy /E /I %TEMPLATE_PATH% !VM_DIR! >nul
+    xcopy /E /I %TEMPLATE_PATH% !VM_DIR!  >> log\run.log  2>&1
     echo  displayName = "macos10.15_%%i" >>  "!VM_DIR!\macos10.15.vmx"
 	echo  sata0:0.fileName = "macos10.15_%%i.vmdk"  >>  "!VM_DIR!\macos10.15.vmx"
 	echo  nvram = "macos10.15_%%i.nvram"  >>  "!VM_DIR!\macos10.15.vmx" 
-	echo  extendedConfigFile = "macos10.15_%%i.vmxf"   >>  "!VM_DIR!\macos10.15.vmx" 
+	echo  extendedConfigFile = "macos10.15_%%i.vmxf" >> "!VM_DIR!\macos10.15.vmx" 
 	rem  iso\Installer_macOS_Catalina_10.15.7.iso
 	echo sata0:1.fileName = "%ISO_BASE_HOME%\Installer_macOS_Catalina_10.15.7.iso"  >>  "!VM_DIR!\macos10.15.vmx" 
-	echo  memsize="2048"  >>   "!VM_DIR!\macos10.15.vmx"
+	echo  memsize="2048" >> "!VM_DIR!\macos10.15.vmx"
 	REM nvram = "macOS 10.15.nvram"  bios  
 	REM displayName = "macOS 10.15"
 	REM sata0:0.fileName = "macOS 10.15.vmdk"
 	REM extendedConfigFile = "macOS 10.15.vmxf"
-	move "!VM_DIR!\macos10.15.vmx"  "!VM_DIR!\macos10.15_%%i.vmx"  >> log\run.log  2>&1
-	move  "!VM_DIR!\macos10.15.vmdk"    "!VM_DIR!\macos10.15_%%i.vmdk"  >> log\run.log  2>&1
-	move  "!VM_DIR!\macos10.15.vmxf"    "!VM_DIR!\macos10.15_%%i.vmxf" >> log\run.log  2>&1
-	move  "!VM_DIR!\macos10.15.nvram"    "!VM_DIR!\macos10.15_%%i.nvram" >> log\run.log  2>&1
+	move "!VM_DIR!\macos10.15.vmx" "!VM_DIR!\macos10.15_%%i.vmx"  >> log\run.log  2>&1
+	move "!VM_DIR!\macos10.15.vmdk" "!VM_DIR!\macos10.15_%%i.vmdk"  >> log\run.log  2>&1
+	move "!VM_DIR!\macos10.15.vmxf" "!VM_DIR!\macos10.15_%%i.vmxf" >> log\run.log  2>&1
+	move "!VM_DIR!\macos10.15.nvram" "!VM_DIR!\macos10.15_%%i.nvram" >> log\run.log  2>&1
     echo Created VM_%%i at !VM_DIR!
 	echo "Starting  launching virtual machines VM_%%i , please wait   ......................."
-	"C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"    start  !VM_DIR!\macos10.15_%%i.vmx 
+	%VMRUN_PATH%  start  !VM_DIR!\macos10.15_%%i.vmx 
 	rem   nogui
 )
 echo All virtual machines have been created and are waiting to be started ................... 
 rem   the virtual machine to starting   please wait    .................... 
 rem   Wait for the VM to start up and obtain the IP address of the VM
-timeout /t 240 
-echo The IP address is being scanned, please wait    .................... 
+rem  timeout /t 240 
+echo The IP address is being scanned, please wait .................... 
 REM  Define the range of nmap scan addresses  for Example   192.168.119.2-10
-for /f "tokens=1,2,3,4 delims=." %%a in ("%vmnet_ip%") do (
-	set /a result_d=%%d + %num%
-	set new_vmnet_ip=%%a.%%b.%%c.!result_d!
-)
-echo !new_vmnet_ip!-%nmap_count%
-nmap !new_vmnet_ip!-%nmap_count%  > nmap\example.txt 
-awk -F: "/report/{ print $0 }" nmap\example.txt | awk -F" " "{ print $NF }"  > nmap\ip_list.txt  
-sed -i  "s/(//g"  nmap\ip_list.txt 
-sed -i  "s/)//g"  nmap\ip_list.txt
-del sed*
+rem for /f "tokens=1,2,3,4 delims=." %%a in ("%vmnet_ip%") do (
+rem 	set /a result_d=%%d + %num%
+rem 	set new_vmnet_ip=%%a.%%b.%%c.!result_d!
+rem )
+rem  echo !new_vmnet_ip!-%nmap_count%
+rem  nmap !new_vmnet_ip!-%nmap_count%  > %IP_PATH%\example.txt 
+rem awk -F: "/report/{ print $0 }" %IP_PATH%\example.txt | awk -F" " "{ print $NF }"  > %IP_PATH%\ip_list.txt  
+rem sed -i  "s/(//g"  %IP_PATH%\ip_list.txt 
+rem sed -i  "s/)//g"  %IP_PATH%\ip_list.txt
+rem  del sed*
 rem  echo  "IP address information is saved in this text  ip_list.txt"
-echo nmap ip address  is done   ....................
+rem echo nmap ip address  is done   ....................
+echo  Obtaining the IP address of the VM is in progress!......................
+:check
+call ip_find.bat
+rem  echo %errorlevel%
+if %errorlevel% neq 0 (
+echo  The VM IP was not found  Waiting for 10 seconds, press a key to continue... ......................
+timeout /t 10
+call get_VM_IPAddress.bat
+goto check     
+) else (
+echo echo  IP address information is saved in %IP_PATH%\ip_list.txt!......................
+)
 echo Start executing scripts in batches  ............................
 rem  VM_COUNT  Number of virtual machines
 rem  Set the number of config.plist to be used
-for /f "tokens=* delims=" %%a in (nmap\ip_list.txt) do (
+for /f "tokens=* delims=" %%a in (%IP_PATH%\ip_list.txt) do (
 	set /a plist_num+=1
 	REM   ssh  -o StrictHostKeyChecking=no  cc@192.168.122.190  '/Users/wx/auto_install.sh'
 	ssh  -o StrictHostKeyChecking=no  wx@%%a   '/Users/wx/mount_efi.sh'  >> log\run.log  2>&1
