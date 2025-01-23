b218f5680f097cfc210973ffc9f9602519a46e80
xiaowen1448 <wenxiaomst@outlook.com>
2025-01-23 17:50:45 +0800

    删除vmxf文件，新增动态获取虚拟机脚本，bug已修复
    删除vmxf文件，新增动态获取虚拟机脚本，bug已修复

--- bat/disable_appleAlert.bat
+++ bat/disable_appleAlert.bat
@@ -6,7 +6,8 @@ set TEMPLATE_PATH=%BASE_DIR%\..\TemplateVM\macos10.15
 REM Set the storage path of the target VM
 set VM_BASE_PATH=%BASE_DIR%\..\NewVM
 set ISO_BASE_HOME=%BASE_DIR%\..\iso
-set IP_PATH=%BASE_DIR%\ip
+set IP_PATH=%BASE_DIR%\var_files
+set LOG_PATH=%BASE_DIR%\var_files
 REM Set the number of virtual machines that need to be created
 set VM_COUNT=4
 set VMRUN_PATH="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
@@ -23,7 +24,7 @@ set plist_num=0
 echo  Obtaining the IP address of the VM is in progress!......................
 rem :check
 rem  goto check 
-copy /y nul %IP_PATH%\ip_list.txt  >> log\run.log  2>&1
+copy /y nul %IP_PATH%\ip_list.txt  >> %LOG_PATH%\run.log  2>&1
 call get_vm_ip.bat
 type %IP_PATH%\ip_list.txt
 echo Start executing scripts in batches  ............................
