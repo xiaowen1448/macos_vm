0e871cc05f2c415d709e20eee609418e3d9053f7
xiaowen1448 <wenxiaomst@outlook.com>
2025-02-07 10:33:15 +0800

    rebuild_nvram.bat
    rebuild_nvram.bat脚本变更

--- bat/rebuild_nvram.bat
+++ bat/rebuild_nvram.bat
@@ -5,9 +5,10 @@ set TEMPLATE_PATH=%BASE_DIR%\..\TemplateVM\macos10.15
 REM Set the storage path of the target VM
 set VM_BASE_PATH=%BASE_DIR%\..\NewVM
 REM Set the number of virtual machines that need to be created
+SET RUN_BAT=%BASE_DIR%\utilities_bat
 set VM_COUNT=4
 set VMRUN_PATH="C:\Program Files (x86)\VMware\VMware Workstation\vmrun.exe"
-rem  stop  vms 
+rem  stop  vms
 call stop_all_vms.bat
 rem  copy  nvram  
 for /r "%VM_BASE_PATH%" %%f in (*.*) do (
@@ -24,4 +25,4 @@ for /r "%VM_BASE_PATH%" %%f in (*.*) do (
 )
 call start_all_vms.bat
 echo  "All virtual machines NVRM have been rebuilt ..............""
-
+pause 
