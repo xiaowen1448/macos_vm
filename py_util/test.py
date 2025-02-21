11765b454ba0917909901272608c4ce976193065
xiaowen1448 <wenxiaomst@outlook.com>
2025-02-21 16:19:26 +0800

    增加test脚本

---
+++ py_util/test.py
@@ -0,0 +1,13 @@
+import list_vm_files
+import get_vm_ip
+vmrun="C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe"
+if __name__ == "__main__":
+    # 示例路径，替换为实际虚拟机文件路径
+    directory = "D:\\macos_vm\\NewVM"  # 替换为你的目录路径
+    vmx_files = list_vm_files.find_vmx_files(directory)
+    # 输出所有找到的 .vmx 文件
+    for vmx in vmx_files:
+        print(f"Found VMX file: {vmx}")
+        vm_path =vmx
+        vm_ip = get_vm_ip.find_vm_ip(vmrun,vm_path)
+        print(f"VM IP Address: {vm_ip}")
