cd565c7fe5532e69c4ed7479f523b6c7a7eba58c
xiaowen1448 <wenxiaomst@outlook.com>
2025-02-26 12:16:05 +0800

    run.cmd
    run.cmd

--- py_util/util_scp_plist.py
+++ py_util/util_scp_plist.py
@@ -3,7 +3,7 @@ def scp_plist(host, username,file_local_path,remote_file_path):
     """使用系统 ssh 命令测试 SSH 登录"""
     try:
         result = subprocess.run(
-            ["scp",file_local_path,f"{username}@{host}:{remote_file_path}"],
+            ["scp","-o", "StrictHostKeyChecking=no",file_local_path,f"{username}@{host}:{remote_file_path}"],
             check=True
         )
         if result.returncode == 0:
