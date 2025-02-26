54e9d044841273b4429fb5d1ba5546cfede9b9e9
xiaowen1448 <wenxiaomst@outlook.com>
2025-02-26 15:32:24 +0800

    cmd中调用python，不在调用clone脚本
    cmd中调用python，不在调用clone脚本

--- py_util/run.cmd
+++ py_util/run.cmd
@@ -1,11 +1,2 @@
-rem  调用五码生成配置文件
-rem  虚拟机克隆的数量要和五码数量一致,确保五码充足可用
-cd  /d ..\plist\
-rem  开始读取五码配置文件，开始批量生成成品plist
-call   ..\plist\run_plist.bat
-rem  开始执行自动克隆，更改ju值，分配五码
-
-cd  /d ..\bat
-call   ..\bat\run_vm_clone.bat
 
 python  test.py
