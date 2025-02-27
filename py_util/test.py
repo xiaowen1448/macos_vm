26f473622972ff9f58948dfd9cc3ecc8876171df
xiaowen1448 <wenxiaomst@outlook.com>
2025-02-27 17:30:21 +0800

    新增五码判断信息，确保每台唯一，虚拟机完成
    新增五码判断信息，确保每台唯一，虚拟机完成

--- py_util/test.py
+++ py_util/test.py
@@ -7,6 +7,8 @@ import time
 import util_vmx_ctrl
 import shutil
 import util_scp_plist
+import  os
+import util_ju_all_unique
 vmrun="C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe"
 ssh_username="wx"
 str_CGSSessionScreenLockedTime="~/CGSSessionScreenLockedTime.sh"
@@ -19,6 +21,7 @@ str_disable_appleAlert="~/disable_appleAlert.sh"
 str_auto_send_vmkey="~/auto_send_key.sh"
 str_reboot="~/reboot.sh"
 str_mount_efi="~/mount_efi.sh"
+str_run_debug="~/run_debug.sh"
 plist_num=1
 remote_plist_dir="/Volumes/EFI/CLOVER/config.plist"
 local_plist_dir=f"D:\\macos_vm\\plist\\chengpin\\config_{plist_num}.plist"
@@ -35,6 +38,8 @@ def test():
     vmx_files = util_vmx.find_vmx_files(vm_directory,".vmx")
     inum = 0
     for_len=len(vmx_files)
+    string_array = []
+    cleaned_array = []
     # 输出所有找到的 .vmx 文件vmx_files = util_vmx.find_vmx_files(directory,".vmx")
     for vmx in vmx_files:
         print(f"Found VMX file: {vmx}")
@@ -58,7 +63,7 @@ def test():
                     3.执行挂在efi分区，注入五码脚本，重建nvrm，
                     4.讲提前生成的五码plist成品配置文件，使用scp拷贝到远端目录。
                     5.由于重新安装macos后，引导顺序会更改，需要将clover设置为第一引导，
-                      直接将提前编辑好的nvram文件复制到每台虚拟机即可，复制旗舰防止文件被占用，需要将虚拟机停机，拷贝后，再次开机。
+                      直接将提前编辑好的nvram文件复制到每台虚拟机即可，复制期间防止文件被占用，需要将虚拟机停机，拷贝后，再次开机。
                     '''
                     util_cmd.execute_ssh_command(vm_ip, ssh_username, str_auto_send_vmkey)
                     util_cmd.execute_ssh_command(vm_ip, ssh_username, str_disable_appleAlert)
@@ -72,28 +77,60 @@ def test():
                     print(f"虚拟机{vm_path}nvram已经重建")
                     util_vmx_ctrl.ctrl_vm(vmrun, "start", vm_path)
                     print(f"虚拟机{vm_path}已经启动!")
-                    continue
-                else:
-                    '''
-                    此处判断为获取finder进程，系统为启动状态，或者是启动后配置状态，无锁屏，代表未配置完成，或者是系统启动后，自动登录已完成
-                    '''
-                    print(f"没有匹配到窗体时间戳，虚拟机{vmx}系统正在启动，请等待！")
-                    str_finder_ps = util_str.contains_substring(" ".join(util_cmd.execute_ssh_command(vm_ip, ssh_username, sh_name_finder)), str_finder)
-                    print(f"{str_finder_ps}")
-                    print(f"匹配到Finder进程，虚拟机{vmx}系统正在安装配置!")
-                    time.sleep(4)
                     if for_len == inum:
                         # 整个for循环执行完毕，仍然错误，代表虚拟机无法获取ip，整个for循环全部重新执行
-                        print(f"{inum}====================={for_len}")
+                        print(f"{inum}++++++++++++++++++++++++++++++{for_len}")
                         test()
                     else:
                         # 整个for循环未执行完毕，仍然错误，代表虚拟机无法获取ip，继续执行剩余for循环
-                        print(f"{inum}------------------------{for_len}")
+                        print(f"{inum}&&&&&&&&&&&&&&&&&&&&&&&&&{for_len}")
                         continue
+                else:
+                    '''
+                                       此处判断为获取finder进程，系统为启动状态，或者是启动后配置状态，无锁屏，代表未配置完成，
+                                       或者是系统启动后，自动登录已完成，
+                                       第一阶段，为执行安装前，此时的ju值每台都是一样的，执行安装脚本后，会重启断卡几十分钟。
+                                       此时没有窗口时间戳，则继续进行循环体
+ 
+                                       第二阶段为，安装完成执行完脚本后，重启后自动登录界面，此时五码和ju均已更改，
+                                       使用脚本获取判断是否更改成功，
+                                       思路：
+                                       执行脚本获取debug输出，判断每台的五码和ju值是否唯一，如成功则结束循环体，如不成功，则继续循环
+                                       '''
+                    print(f"没有匹配到窗体时间戳，虚拟机{vmx}系统正在启动，请等待！")
+                    str_finder_ps = util_str.contains_substring(
+                        " ".join(util_cmd.execute_ssh_command(vm_ip, ssh_username, sh_name_finder)), str_finder)
+                    print(f"{str_finder_ps}")
+                    print(f"匹配到Finder进程，虚拟机{vmx}系统正在安装配置!")
+                    '''
+                    开始获取SerialNumber值，调用debug
+                    '''
+                    str_SerialNumber = " ".join(util_cmd.execute_ssh_command(vm_ip, ssh_username, str_run_debug))
+                    print(f"IM_SerialNumber输出如下:{str_SerialNumber}")
+                    ju_files = os.getcwd()
+                    ju_filename = f"{ju_files}\\vm_debug_{inum}.ju"
+                    with open(ju_filename, 'w') as file:
+                        file.write(str_SerialNumber)
+                        # 判断循环体中获取的ju值全部是否唯一，
+                        print(f"{ju_filename}========")
+                        #  for ju in ju_files:
+                        # print(f"{ju}==============================================")
+                    with open(ju_filename, 'rb') as file2:
+                        ju_str = "".join(file2.read().decode())
+                    print(f"虚拟机vm_{inum}获取的SerialNumber:{ju_str}")
+                    string_array.extend([ju_str])
+                    cleaned_array = [s.rstrip() for s in string_array]
             else:
                 print(f"❌ 虚拟机{vmx}系统 SSH 登录失败：十秒后重新尝试")
                 time.sleep(4)
-                test()
+                if for_len == inum:
+                    # 整个for循环执行完毕，仍然错误，代表虚拟机无法获取ip，整个for循环全部重新执行
+                    print(f"{inum}!!!!!!!!!!!!!!!!!!!!!!!!!!!{for_len}")
+                    test()
+                else:
+                    # 整个for循环未执行完毕，仍然错误，代表虚拟机无法获取ip，继续执行剩余for循环
+                    print(f"{inum}@@@@@@@@@@@@@@@@@@@@@@@@@@@@{for_len}")
+                    continue
         else:
             print(f"获取虚拟机{vmx} ip地址失败，十秒后重新尝试获取，系统正在重启中")
             time.sleep(4)
@@ -105,6 +142,28 @@ def test():
                 # 整个for循环未执行完毕，仍然错误，代表虚拟机无法获取ip，继续执行剩余for循环
                 print(f"{inum}------------------------{for_len}")
                 continue
+            cleaned_array.clear()
+
+    print(f"虚拟机的序列号:{cleaned_array}")
+    ary_len=len(cleaned_array)
+    if ary_len<for_len:
+        print(f"部分虚拟机的SerialNumber有空，虚拟机可能在重启")
+        time.sleep(4)
+        test()
+    else:
+        ju_true = util_ju_all_unique.are_all_unique(cleaned_array)
+        if ju_true:
+            print(f"所有虚拟机的SerialNumber均为唯一,所有虚拟机制作完成!")
+            # 删除临时文件
+        # ju_files = util_vmx.find_vmx_files(os.getcwd(), ".ju")
+        # for ju in ju_files:
+        # os.remove(ju)
+        else:
+            print(f"所有虚拟机的SerialNumber有重复，脚本执行中，正在等待重启")
+            time.sleep(4)
+            cleaned_array.clear()
+            test()
+
 
 test()
 
---
+++ py_util/util_ju_all_unique.py
@@ -0,0 +1,3 @@
+def are_all_unique(arr):
+    # 比较原始长度与集合长度
+    return len(arr) == len(set(arr))
